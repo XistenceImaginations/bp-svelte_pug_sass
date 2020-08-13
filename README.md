06d5578775e928ec0c5e49f57ef7f45d48f1d1c9
Kai P. Uecker <k_p.uecker@xistenceimaginations.de>
2020-08-13 09:32:48 +0200

    master: initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Boilerplate for Svelte with Sass and Pug
+
+## Installation
+
+- npm install
+
+## Development
+
+- npm run dev
+
+## Build
+
+- npm run build
---
+++ src/App.svelte
@@ -0,0 +1,15 @@
+<template lang="pug">
+.foobar Foobar
+</template>
+
+<script>
+let name = 'world';
+</script>
+
+<style lang="scss" global>
+.foobar {
+  color: red;
+  font-weight: 700;
+  font-size: 2em;
+}
+</style>
